69a443979ab95fd46c51c2ab1ffa5ebdb8a311ff
chandu078 <chandudyavanapelli03@gmail.com>
2022-06-08 05:55:41 +0000

    martini: Set default hotspot ssid by RRO
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- device.mk
+++ device.mk
@@ -62,7 +62,8 @@ PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
-    OPlusSystemUIResTarget
+    OPlusSystemUIResTarget \
+    OPlus9RTWifiOverlay
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
