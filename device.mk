f8c4daf938bac22c0591b3ebdd7bb634d5fae749
spkal01 <kalligeross@gmail.com>
2022-11-12 01:19:05 +0800

    martini: Set correct front camera id for FaceUnlockService
    Co-authored-by: bheatleyyy <sugakesshaa@pixelos.net>
    Change-Id: Ie20ba329f816a2573f68f0047c32e8cd295bcdef

--- device.mk
+++ device.mk
@@ -52,6 +52,7 @@ PRODUCT_PACKAGES += \
     EvolutionFrameworksResTarget\
     EvolutionSettingsResTarget \
     EvolutionSystemUIResTarget \
+    FaceUnlockServiceMartini \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSystemUIResTarget
