30156e7c064a9dcb3cfc2e9619f35018f8f3115b
Mrick343 <mrinmoy8429de@gmail.com>
2024-05-15 21:32:59 +0530

    martini: Init euclidOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_martini.mk
+    $(LOCAL_DIR)/euclid_martini.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_martini-user \
-    lineage_martini-userdebug \
-    lineage_martini-eng
+    euclid_martini-user \
+    euclid_martini-userdebug \
+    euclid_martini-eng
